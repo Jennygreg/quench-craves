12fa2d0ffc371113bc0638fc4c36a32d6e1156bf
Jennygreg <jennygregory11@gmail.com>
2023-09-23 23:40:27 +0100

    added image to home page

--- src/components/Animatedimg.js
+++ src/components/Animatedimg.js
@@ -1,11 +1,15 @@
 import React from "react";
-import image from "../asset/orderimg.png";
+import image from "../asset/grilledvegcapachio.png";
+import friedrice from "../asset/applepecanchickensalad.png"
+import spice from "../asset/spice.png"
 
 import './Animatedimg.css'
 
 export default function Animatedimg (){
     return(
-    <div> 
+    <div className="pt-5"> 
+       <div><img src={friedrice}alt="classicslad" className="salad"/>
+       <img src={spice}alt="spices" className="spices"/></div> 
      <img src={image} alt ="display" className="displayimg"/>
    
     </div>
--- src/components/Highlight.js
+++ src/components/Highlight.js
@@ -3,9 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCertificate } from '@fortawesome/free-solid-svg-icons';
 import { faSeedling } from '@fortawesome/free-solid-svg-icons';
 import { faTruck } from '@fortawesome/free-solid-svg-icons';
+import pancake from "../asset/pancake.png"
+import pasta from "../asset/pasta.png"
+import pizza from "../asset/pizza.png"
+import chop from "../asset/chops.png"
 import "./Highlight.css"
 export default function Highlight(){
-    return(<div>
+    return(<div className='pt-5 mt-3'>
        <div className='highlight'> 
        
        <div>
@@ -25,7 +29,28 @@ export default function Highlight(){
         <h3>Fast Delivery</h3>
         <p>We delivery very fast </p>
         </div>
+       
+       
         
         </div>
-    </div>)
+        <h2 className='favourites'>Our Favourites</h2>
+        <div className='homepageimg'>
+            
+            <div>
+                <img src={pancake}alt="pancake" className='pancake'/>
+                <p>Pancakes</p>
+            </div>
+            <div>
+                <img src= {pasta} alt="pasta"/>
+                <p>Pasta</p>
+            </div>
+            <div>
+                <img src={pizza}alt="pizza"/>
+                <p>Pizza</p>
+            </div>
+            <div>
+                <img src={chop}alt="chop" className='chop'/>
+                <p>Chops</p>
+            </div></div></div>
+    )
 }
