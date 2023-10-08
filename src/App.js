936395df6b2b496d2a59b07bb101c27733e63dce
Jennygreg <jennygregory11@gmail.com>
2023-10-08 02:40:35 +0100

    created a new account and login form

--- src/App.js
+++ src/App.js
@@ -1,62 +1,32 @@
+import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import Animatedimg from "./components/Animatedimg.js"
+import Navbar from './components/Navbar.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faSquareCheck } from '@fortawesome/free-regular-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import googleimg from './asset/googlelogo.png'
 
 function App() {
+return <div>
+  <Navbar/>
+  <div className='accountForm'>
+   <h1>Create Account</h1> 
+    <form>
+      <dvi className ='d-flex justify-content-evenly'><FontAwesomeIcon icon={faUser} className='icons'/><input type='text'placeholder='Username'className='input' /></dvi>
+    <div className ='d-flex justify-content-evenly'><FontAwesomeIcon icon={faEnvelope} className='icons'/><input type = 'email'placeholder='Email'className='input' /></div>  
+      <div className ='d-flex justify-content-evenly'><FontAwesomeIcon icon={faLock}className='icons'/><input type='password'placeholder='Password' className='input'/></div>
+     <div className ='d-flex justify-content-evenly'><FontAwesomeIcon icon={faSquareCheck}className='icons'/><input type='password' placeholder='Confirm Password'className='input'/></div>
+     <div className='d-flex justify-content-Center accountbtn'><button className='createacct' id='homebtn'> Create </button> 
+      <button className='signUp'  id='homebtn'>Sign In</button></div>
+    </form>
+    <p>Or Sign in with</p>
+     <button className='googleBtn'><img src={googleimg} alt='googlelogo' className='googleIcon'/></button>
+  </div>
+  </div>
+
  
-  return (
-    <div className="App">
-     
-       <div>
-       <nav class="navbar navbar-expand-lg sticky-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="" className='logoimg'alt="logo"/></a>
-    <button class="navbar-toggler shrinkbtn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-   
-    <div class="collapse navbar-collapse d-flex justify-content-end d-none d-sm-none"  id="navbarNavAltMarkup">
-      <div class="navbar-nav ">
-        <a class="nav-link" aria-current="page" href="#">Create Account</a>
-        <a class="nav-link" href="#">Sign in</a>
-        <a class="nav-link" href="#">Contact</a>
-        
-      </div>
-    </div>
-   
-  </div>
-</nav>
-</div >
-
-
-<div className='orderWrapper'>
-<div id="overlayer">
-  <div className='textWrapper'><h1>Make an Order</h1>
-   <p>Not Sign up Yet? click to create account</p> 
-<button className='homebtn create'>
-  Create Account
-</button>
-<p>Already have an account? click to login/signin</p>
-<button className='homebtn signin'>Login/Sign in</button>
-<p> or log in with Google account, click below</p>
-<button className='homebtn google'>Google account</button></div>
-<p className="ani" >The Spot for Satisfaction...!</p>
-  </div> 
-
-<div className='row'>
-
-  <div className="col-6 logins">
-   
-  </div>
-
-
-  <div className="col-6 ">
-<Animatedimg className="aniImg"/>
-  </div>
-</div>
-</div>
-</div>
-  );
 }
 
 export default App;
